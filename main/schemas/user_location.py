cf1c7548b5cd482b07553201b6f018611c0cd452
Codefred <alfredemmanuelinyang@gmail.com>
2020-05-31 14:50:14 +0100

    Fix schema

--- main/schemas/user_location.py
+++ main/schemas/user_location.py
@@ -8,9 +8,9 @@ class UserLocationSchema(Schema):
     location_id = ms_fields.Str()
     date_time = ms_fields.DateTime(default=datetime.datetime.now())
     splitted = ms_fields.Bool()
-    accuracy = ms_fields.Int()
-    location_type = ms_fields.Int()
-    longitude = ms_fields.Int()
+    accuracy = ms_fields.Float()
+    location_type = ms_fields.Float()
+    longitude = ms_fields.Float()
     speed = ms_fields.Int()
     arrival = ms_fields.Bool()
     
